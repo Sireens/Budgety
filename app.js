a3f7ab248e56ed6f25f29ae3c3dbd8f655ce0499
Sireens <guilherme.atu@gmail.com>
2018-05-21 13:53:27 -0300

    Pausa pro almoço :)

--- app.js
+++ app.js
@@ -5,18 +5,19 @@ var budgetController = (function() {
 
 })();
 
-
-
-
-
 // UI Controller
 var UIController = (function() {
 
-  // Some code
-
+  return {
+    getInput: function() {
+      return {
+        type: document.querySelector('.add__type').value, // Will be either inc ou exp
+        description: document.querySelector('.add__description').value,
+        value: document.querySelector('.add__value').value,
+      };      
+    }
+  };
 })();
-
-
 
 // Global APP Controller
 var controller = (function(budgetCrtl, UICtrl){
@@ -25,20 +26,20 @@ var controller = (function(budgetCrtl, UICtrl){
 
     // 1. Get the field input data
 
+    var input = UICtrl.getInput();
+    console.log(input);
+
     // 2. Add the item to the budget controller
 
     // 3. Add the new item to the UI
 
     // 4. Calculate the budget
 
-    // 5. Display the budget on the UI
-
-    console.log('Enter was entered');
+    // 5. Display the budget on the UI  
 
   }
 
   document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
-
   document.addEventListener('keypress', function(event) {
     if(event.keyCode === 13 || event.which === 13) {
       ctrlAddItem();
